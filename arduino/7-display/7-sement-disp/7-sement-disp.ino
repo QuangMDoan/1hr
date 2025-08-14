a63e9ef2357ba2ddf964e122112baf76077faba5
Quang Doan <quang.m.doan@gmail.com>
2025-08-14 10:48:23 -0700

    Reimplement 4-digit display

--- arduino/7-display/7-sement-disp/7-sement-disp.ino
+++ arduino/7-display/7-sement-disp/7-sement-disp.ino
@@ -11,21 +11,49 @@
 #define CATH_3 12 
 #define CATH_4 13
 
-char x[10][9] = {"11010111", 
-                 "00010100",
-                 "11001101", 
-                 "01011101",
-                 "00011110",
-                 "01011011",
-                 "11011010",
-                 "00010101", 
-                 "11011111",
-                 "01011111", 
+#define MIN_PIN 2
+
+char x[10][9] = {"11010111",    // 0
+                  "00010100",   // 1
+                  "11001101",   // 2
+                  "01011101",   // 3
+                  "00011110",   // 4
+                  "01011011",   // 5
+                  "11011010",   // 6
+                  "00010101",   // 7
+                  "11011111",   // 8
+                  "01011111"   // 9
                  };
 
+void setup() {
+  for(int i = MIN_PIN; i <= CATH_4; i++) pinMode(i , OUTPUT);
+  Serial.begin(9600);
+}
 
+void displayDigit(int digit, int pos){
+  for(int i = 0; i < 4; i++) {
+    digitalWrite(CATH_1 + i, pos != i);
+  } 
+
+  for(int i = 0; i < 8; i++) {
+    digitalWrite(MIN_PIN + i, x[digit][i] - '0');
+  }
+}
+
+void displayDigits(int32_t x) {
+  for (int pos =0; pos < 4; pos++) {
+    displayDigit(x%10, pos);
+    delay(4);
+    x /= 10;
+  }
+}
+
+void displayDigitsFor(int32_t x, int ms){
+  uint32_t start = millis();
+  while ((millis() - start) < ms) displayDigits(x);
+}
 
 void loop(){
-  for (int x = 89; x < 2026; x++) 
+  for (int x = 29; x < 2026; x++) 
     displayDigitsFor(x, 500);
 }
