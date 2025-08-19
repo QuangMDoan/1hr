c8adce64ef42eb2c395f99d3c16a05daceb7ca41
Quang Doan <quang.m.doan@gmail.com>
2025-08-18 20:12:15 -0700

    Reimplement the turn function

--- v5-vscode/DriveTrain01/FourMotorBot1/src/main.cpp
+++ v5-vscode/DriveTrain01/FourMotorBot1/src/main.cpp
@@ -22,11 +22,6 @@ void initializeRandomSeed(){
 void vexcodeInit() {
   initializeRandomSeed(); 
 }
-
-void playVexcodeSound(const char *soundName) {
-  printf("VEXPlaySound:%s\n", soundName);
-  wait(5, msec);
-}
   
 struct MotorGroup {
   std::vector<motor> motors;
@@ -72,8 +67,13 @@ struct MotorGroup {
 
 class MyDriveTrain {
 public:  
+  // wheel revs = turns * (t^2 + b^2) * pi / (c * t)
+  // t: track width 
+  // b: wheel base
+  // c: wheel circumference
+
   const double WheelDiameter = 4 * INCH2METERS; 
-  const double TrackWidth = 14.5 * INCH2METERS;
+  const double TrackWidth = 14.0 * INCH2METERS;
   const double WheelBase = 40 * 0.001;
 
   void setup(){
@@ -88,17 +88,23 @@ public:
     leftMotorGroup.setSpeed(x);
   }
 
+  double getLeftWheelRevolutions(){
+    double leftRotations = leftMotorGroup.getRotations();
+    // Brain.Screen.printAt(5, 40, "left rotations: %.3f ", leftRotations);
+    return leftMotorGroup.getRotations();
+  }
+
   double getLeftDistanceMeters(){
     double leftRotations = leftMotorGroup.getRotations();
     double leftMeters = WheelDiameter * M_PI * leftRotations; 
-    Brain.Screen.printAt(10, 50, "left rotations: %.3f, meters: %.3f", leftRotations, leftMeters);
+    Brain.Screen.printAt(5, 40, "left rotations: %.4f, meters: %.4f", leftRotations, leftMeters);
     return leftMeters;
   }
 
   double getRightDistanceMeters(){
     double rightRotations = rightMotorGroup.getRotations();
     double rightMeters = WheelDiameter * M_PI * rightRotations; 
-    Brain.Screen.printAt(10, 100, "right rotations: %.3f, meters: %.3f", rightRotations, rightMeters);
+    Brain.Screen.printAt(5, 80, "right rotations: %.4f, meters: %.4f", rightRotations, rightMeters);
     return rightMeters;
   }
 
@@ -123,13 +129,28 @@ public:
     rightMotorGroup.resetMeters();
     leftMotorGroup.resetMeters();
     Brain.Screen.clearScreen();
-    double chasisTurns = x / 360;
-    double chasisTurnArcLength = chasisTurns * TrackWidth * M_PI;
-    Brain.Screen.printAt(10, 150, "degree: %.3f, turn: %.3f, Arc %.3f", x, chasisTurns, chasisTurnArcLength);
-    while (getLeftDistanceMeters() < chasisTurnArcLength) {
+    
+    double turns = x / 360;    
+    double c = WheelDiameter * M_PI;
+    double b = WheelBase;
+    double t = TrackWidth;
+
+    // double arcLength = turns * t * M_PI;
+    // while (getLeftDistanceMeters() < arcLength) {
+    //   rightMotorGroup.drive(false);
+    //   leftMotorGroup.drive();
+    // }
+
+    // wheel revs = turns * (t^2 + b^2) * pi / (c * t)
+    double wheel_revs = turns * (t*t + b*b) * M_PI / (c * t);
+    Brain.Screen.printAt(5, 120, "Target %.4f (%.4f revs)", x, turns);
+
+    for (double revs = getLeftWheelRevolutions(); revs < wheel_revs; revs = getLeftWheelRevolutions()) {
       rightMotorGroup.drive(false);
       leftMotorGroup.drive();
+      Brain.Screen.printAt(5, 160, "target wheel revs: %.4f, current revs: %.4f", wheel_revs, revs);
     }
+
     leftMotorGroup.stopMotors();
     rightMotorGroup.stopMotors();
   }
@@ -144,5 +165,16 @@ int main() {
   MyDriveTrain driveTrain;
   driveTrain.setup();
   driveTrain.setSpeed(25);
+
   driveTrain.turnForDegrees(180);
+  wait(1, seconds);
+
+  driveTrain.turnForDegrees(90);
+  wait(1, seconds);
+
+  driveTrain.turnForDegrees(90);
+  wait(60, seconds);
+
+  driveTrain.turnForDegrees(720);
+  wait(1, seconds);
 }
