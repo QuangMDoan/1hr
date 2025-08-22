6258a0555b77bf6c9b1f1c3acd5f3f484790951d
Quang Doan <quang.m.doan@gmail.com>
2025-08-22 13:29:33 -0700

    Clean up code

--- v5-vscode/MecanumDrivetrain/include/drive_train.h
+++ v5-vscode/MecanumDrivetrain/include/drive_train.h
@@ -11,9 +11,6 @@ using namespace vex;
 class MecanumDriveTrain {
 public:  
   // wheel revs = turns * (t^2 + b^2) * pi / (c * t)
-  // t: track width 
-  // b: wheel base
-  // c: wheel circumference
 
   const double WheelDiameter = 2 * INCH2METERS; 
   const double TrackWidth = 14.0 * INCH2METERS;
@@ -30,49 +27,6 @@ public:
     frontRight_.setVelocity(x, pct);
     backRight_.setVelocity(x, pct);
   }
-
-//   void driveForMeters(double x){
-//     leftMotorGroup.resetMeters();
-//     rightMotorGroup.resetMeters();
-//     Brain.Screen.clearScreen();
-
-//     while(getDistanceMeters() < x) {
-//       leftMotorGroup.drive();
-//       rightMotorGroup.drive();
-//     }
-//     leftMotorGroup.stopMotors();
-//     rightMotorGroup.stopMotors();
-//   }
-
-//   void turnForDegrees(double x) {
-//     rightMotorGroup.resetMeters();
-//     leftMotorGroup.resetMeters();
-//     Brain.Screen.clearScreen();
-    
-//     double turns = x / 360;    
-//     double c = WheelDiameter * M_PI;
-//     double b = WheelBase;
-//     double t = TrackWidth;
-
-//     // double arcLength = turns * t * M_PI;
-//     // while (getLeftDistanceMeters() < arcLength) {
-//     //   rightMotorGroup.drive(false);
-//     //   leftMotorGroup.drive();
-//     // }
-
-//     // wheel revs = turns * (t^2 + b^2) * pi / (c * t)
-//     double wheel_revs = turns * (t*t + b*b) * M_PI / (c * t);
-//     Brain.Screen.printAt(5, 120, "Target %.4f (%.4f revs)", x, turns);
-
-//     for (double revs = getLeftWheelRevolutions(); revs < wheel_revs; revs = getLeftWheelRevolutions()) {
-//       rightMotorGroup.drive(false);
-//       leftMotorGroup.drive();
-//       Brain.Screen.printAt(5, 160, "target wheel revs: %.4f, current revs: %.4f", wheel_revs, revs);
-//     }
-
-//     leftMotorGroup.stopMotors();
-//     rightMotorGroup.stopMotors();
-//   }
 
   void turnLeftForDeg(double deg) {
     turnForRotations(deg * ROTPERDEG, true);
